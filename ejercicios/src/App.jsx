40e80193e79c4ea142266afafac0372ab619d9b1
unknown <alex.chinchi02@gmail.com>
2025-02-12 17:18:13 +0100

    cmabios

---
+++ ejercicios/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Boton from './components/Boton'
+
+function App() {
+  const [suma, setSuma] = useState(0)
+
+  return (
+    <>
+      <Boton></Boton>
+    </>
+  )
+}
+
+export default App
---
+++ ejercicios/src/components/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Boton = () => {
+  return (
+    <>
+        <button className='rounded-full font-bold text-2xl'>+</button>
+        <button>-</button>
+    </>
+  )
+}
+
+export default Boton
