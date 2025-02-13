b9cf1031e063f25b67575079cbee81c90c6095ba
unknown <alex.chinchi02@gmail.com>
2025-02-13 09:53:21 +0100

    ca

--- tiendaApp/src/App.jsx
+++ tiendaApp/src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Plantilla from './layout/Plantilla'
+import Detalles from './pages/Detalles'
+import Error from './pages/Error'
+import Contacto from './pages/Contacto'
+import Home from './pages/Home'
+import Productos from './pages/Productos'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-8xl'>Hola mundo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Plantilla />}>
+            <Route index element={<Home />} />
+            <Route path='/productos' element={<Productos />} />
+            <Route path='productos/:id' element={<Detalles />} />
+            <Route path='/contacto' element={<Contacto />} />
+            <Route path='*' element={<Error/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ tiendaApp/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <h1>Etsás más perdido que Perro Sanchez</h1>
+  )
+}
+
+export default Error
